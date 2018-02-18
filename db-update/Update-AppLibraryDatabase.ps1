9859e9d82b57012a48de6862e1c55c9569330ce8
Tobias Kiertscher <dev@mastersign.de>
2018-02-18 15:01:32 +0100

    introduced caching of downloaded Bench archive

--- db-update/Update-AppLibraryDatabase.ps1
+++ db-update/Update-AppLibraryDatabase.ps1
@@ -7,6 +7,13 @@ param (
 )
 $Script:thisDir = Split-Path $MyInvocation.MyCommand.Path -Parent
 
+# Prepare download directory
+$downloadDir = "$Script:thisDir\downloads"
+if (!(Test-Path $downloadDir))
+{
+    mkdir $downloadDir | Out-Null
+}
+
 # Prepare working directory
 $workDir = "$Script:thisDir\tmp"
 if (Test-Path $workDir)
@@ -24,25 +31,41 @@ if (!$data.assets)
     Write-Error "Downloading the latest release info failed."
     exit 1
 }
-$archiveUrl = $data.assets `
-    | ? { $_.name -eq "Bench.zip" } `
-    | % { $_.browser_download_url }
-if (!$archiveUrl)
-{
-    Write-Error "Failed to retrieve URL of Bench.zip"
-    exit 1
-}
 
-# Download Bench.zip
-Invoke-WebRequest $archiveUrl -UseBasicParsing -OutFile "$workDir\Bench.zip"
-[string]$benchDir = mkdir .\bench
+$release = $data.name
+echo "Latest release: $release"
+
+# Check cache
+[string]$cachedRelease = Get-Content "$downloadDir\version.txt" -ErrorAction SilentlyContinue
+if ($release -ne $cachedRelease)
+{
+    echo "Downloading Bench release $release"
+    $archiveUrl = $data.assets `
+        | ? { $_.name -eq "Bench.zip" } `
+        | % { $_.browser_download_url }
+    if (!$archiveUrl)
+    {
+        Write-Error "Failed to retrieve URL of Bench.zip"
+        exit 1
+    }
+
+    # Download Bench.zip
+    Invoke-WebRequest $archiveUrl -UseBasicParsing -OutFile "$downloadDir\Bench.zip"
+
+    # Save downloaded release version
+    $release | Set-Content "$downloadDir\version.txt"
+}
+else
+{
+    echo "Using Bench $release from cache"
+}
 
 # Extract Bench.zip
 try
 {
     $ws = New-Object -ComObject Shell.Application
-    $zip = $ws.NameSpace("$workDir\Bench.zip")
-    $trg = $ws.NameSpace("$workDir\bench")
+    $zip = $ws.NameSpace("$downloadDir\Bench.zip")
+    $trg = $ws.NameSpace("$workDir")
     foreach ($item in $zip.items())
     {
         $trg.copyhere($item, 0x14)
@@ -56,13 +79,13 @@ catch
 }
 
 # Initialize Bench environment for app library download
-mkdir "$benchDir\config" | Out-Null
-copy "$benchDir\res\config.template.md" "$benchDir\config\config.md"
-"" | Out-File "$benchDir\config\apps-activated.txt" -Encoding Default
-"" | Out-File "$benchDir\config\apps-deactivated.txt" -Encoding Default
+mkdir "$workDir\config" | Out-Null
+copy "$workDir\res\config.template.md" "$workDir\config\config.md"
+"" | Out-File "$workDir\config\apps-activated.txt" -Encoding Default
+"" | Out-File "$workDir\config\apps-deactivated.txt" -Encoding Default
 
 # Put Bench CLI on Path
-$env:PATH = "$benchDir\auto\bin;$env:PATH"
+$env:PATH = "$workDir\auto\bin;$env:PATH"
 
 # Download and extract app libraries
 bench --verbose manage load-app-libs
@@ -73,10 +96,10 @@ if (!$?)
 }
 
 # Load Bench PowerShell API
-. "$benchDir\auto\lib\bench.lib.ps1"
+. "$workDir\auto\lib\bench.lib.ps1"
 
 # Load Bench configuration
-$cfg = New-Object Mastersign.Bench.BenchConfiguration ($benchDir, $true, $true, $false)
+$cfg = New-Object Mastersign.Bench.BenchConfiguration ($workDir, $true, $true, $false)
 
 # Enumerate all apps
 $no = 0
